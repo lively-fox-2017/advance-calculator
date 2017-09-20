3d5123deb9ff425ad13f478a9815bf55be2d0c13
lokiloko <ianw.tkj2@gmail.com>
2017-09-20 17:55:10 +0700

    add feature add,substract,multiply,divide,square,squareroot

--- calculator.js
+++ calculator.js
@@ -2,19 +2,32 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (defaultNum = 0) {
+    this.defaultNum = defaultNum;
   }
-  add () {
+  add (x) {
+    this.defaultNum += x;
+    return this;
   }
-  substract () {
+  substract (x) {
+    this.defaultNum -= x;
+    return this;
   }
-  multiply () {
+  multiply (x) {
+    this.defaultNum *= x;
+    return this;
   }
-  divide () {
+  divide (x) {
+    this.defaultNum /= x;
+    return this;
   }
   square () {
+    this.defaultNum *= this.defaultNum;
+    return this;
   }
   squareRoot () {
+    this.defaultNum = Math.sqrt(this.defaultNum);
+    return this;
   }
 }
 
@@ -25,7 +38,9 @@ class Calculator {
 * - Template Literals
 * - Method Chaining
 */
-
+var calc = new Calculator(0);
+calc.add(10).multiply(10).squareRoot().square();
+console.log(calc);
 module.exports = {
   Calculator
 }
