855cfa08933f01704e915a6ea6b420e42d81d861
chandrabuwana <cbuwana@gmail.com>
2017-09-21 20:51:02 +0700

    calculator metode chainig

--- calculator.js
+++ calculator.js
@@ -2,21 +2,41 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (num) {
+    this.num=num
   }
-  add () {
+  add (tambah) {
+    this.num=this.num+tambah
+    return this
   }
-  substract () {
+  substract (kurang) {
+    this.num=this.num-kurang
+    return this
   }
-  multiply () {
+  multiply (kali) {
+    this.num=this.num*kali
+    return this
   }
-  divide () {
+  divide (bagi) {
+    this.num=this.num/bagi
+    return this
   }
-  square () {
+  square (pangkat) {
+    this.num=Math.pow(this.num,pangkat)
+    return this
   }
-  squareRoot () {
+  squareRoot (akar) {
+    this.num=Math.sqrt(this.num)
+    return this
+  }
+  phi(phi){
+    this.num=Math.PI*(this.num+this.num)
+    return this
   }
 }
+var num=new Calculator(50)
+console.log(num);
+console.log(num.add(10).substract(10).multiply(4).divide(10).square(2).squareRoot(9).phi(21));
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
