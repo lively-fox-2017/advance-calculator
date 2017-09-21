15abe575dda4ab19672c9becd084f0a82c02f2e0
munawirali <munawirali@gmail.com>
2017-09-21 11:08:53 +0700

    done

--- calculator.js
+++ calculator.js
@@ -2,22 +2,47 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (nilai1) {
+    this.nilai1=nilai1;
   }
-  add () {
+  //penambahan
+  add (nilai2) {
+    this.nilai1+=nilai2;
+    return this;
   }
-  substract () {
+  //pengurangan
+  substract (nilai2) {
+    this.nilai1-=nilai2;
+    return this;
   }
-  multiply () {
+  //perkalian
+  multiply (nilai2) {
+    this.nilai1*=nilai2;
+    return this;
   }
-  divide () {
+  //pembagian
+  divide (nilai2) {
+    this.nilai1/=nilai2;
+    return this
   }
-  square () {
+  //perpangkatan
+  squareRoot(nilai2) {
+    this.nilai1=Math.pow(this.nilai1,nilai2);
+    return this;
   }
-  squareRoot () {
+  //luas lingkaran
+  circleArea() {
+    this.nilai1=(2*Math.PI*this.nilai1);
+    return this;
+  }
+
+  print(){
+    return this.nilai1;
   }
 }
-
+let calculator=new Calculator(2);
+// console.log(calculator.squareRoot(78).print())
+console.log(calculator.add(2).substract(1).print())
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters
