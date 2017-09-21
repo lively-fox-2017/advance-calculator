8ef8ad6b84f9ecd214e2b7eb9c249b4d33af1d11
aditReza <lete.note3@gmail.com>
2017-09-21 18:35:51 +0700

    add addNum-method

--- calculator.js
+++ calculator.js
@@ -1,31 +1,29 @@
 'use strict'
 
 class Calculator {
-  //write your code here
-  constructor () {
-  }
-  add () {
-  }
-  substract () {
-  }
-  multiply () {
-  }
-  divide () {
-  }
-  square () {
-  }
-  squareRoot () {
-  }
+    //write your code here
+    constructor() {
+        this.num = 0;
+    }
+    add(numAdd) {
+        this.num += numAdd
+        return this
+    }
+    substract() {}
+    multiply() {}
+    divide() {}
+    square() {}
+    squareRoot() {}
 }
 
 /** note : you can use several features from ecmascript, such as:
-* - Classes
-* - Default Parameters
-* - Destructured Assignment
-* - Template Literals
-* - Method Chaining
-*/
+ * - Classes
+ * - Default Parameters
+ * - Destructured Assignment
+ * - Template Literals
+ * - Method Chaining
+ */
 
 module.exports = {
-  Calculator
+    Calculator
 }
