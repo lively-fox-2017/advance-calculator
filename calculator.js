caaa9413ef90391f54d999cd2e23b42befa18803
azhariemuhammad <azharie_muhammad@live.com>
2017-09-21 17:37:49 +0700

    dah seles

--- calculator.js
+++ calculator.js
@@ -3,21 +3,47 @@
 class Calculator {
   //write your code here
   constructor () {
+    this.number = 0
   }
-  add () {
+  add (i) {
+    this.number += i
+    return this
   }
-  substract () {
+  substract (i) {
+    this.number -=  i
+    return this
   }
-  multiply () {
+  multiply (i) {
+    this.number *= i
+    return this
   }
-  divide () {
+  divide (i) {
+    this.number /= i
+    return this
   }
-  square () {
+  square (i) {
+    
+    this.number = Math.pow(i,2)
+    return this
   }
-  squareRoot () {
+  squareRoot (i) {
+    this.number = Math.sqrt(i)
+    return this
+  }
+
+  print () {
+  return this.number
   }
 }
 
+let Calc = new Calculator ()
+
+// //Calc.add(12)
+// Calc.multiply(2)
+// //Calc.squareRoot(2)
+// console.log(Calc.print())
+
+console.log(Calc.add(2).multiply(2).print());
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters
