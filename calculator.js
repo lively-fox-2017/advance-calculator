fd7bd75eb6518021126ca1addd8eeabf7ce93ac9
Dimitri <deathmitri@gmail.com>
2017-09-21 12:37:23 +0700

    Finished advanced-calculator

--- calculator.js
+++ calculator.js
@@ -1,21 +1,76 @@
 'use strict'
 
 class Calculator {
-  //write your code here
+
   constructor () {
+
+    this.pi = 22/7;
+    this.result = 0;
+
   }
-  add () {
+
+  add (num) {
+
+    this.result += num;
+
+    return this;
+
   }
-  substract () {
+
+  substract (num) {
+
+    this.result -= num;
+
+    return this;
+
   }
-  multiply () {
+
+  multiply (num) {
+
+    this.result *= num;
+
+    return this;
+
   }
-  divide () {
+
+  divide (num) {
+
+    this.result /= num;
+
+    return this;
+
   }
+
   square () {
+
+    this.result = Math.pow(this.result, 2);
+
+    return this;
+
   }
+
+  pow(num) {
+
+    this.result = Math.pow(this.result, num);
+
+    return this;
+
+  }
+
   squareRoot () {
+
+    this.result = Math.sqrt(this.result);
+
+    return this;
+
   }
+
+  printResult() {
+
+    console.log(this.result);
+
+  }
+
 }
 
 /** note : you can use several features from ecmascript, such as:
--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 'use strict'
 
-const {Calculator} = require("./calculator.js")
+const {Calculator} = require("./calculator.js");
 
 // execute function on calculator.js in here
+
+let calc = new Calculator;
+
+calc.add(2).pow(3).substract(2).divide(2);
+
+calc.printResult();
+
+let areaOfCircle = new Calculator;
+
+areaOfCircle.add(7).square().multiply(areaOfCircle.pi);
+
+areaOfCircle.printResult();
