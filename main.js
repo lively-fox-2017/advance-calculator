8ae9a78ac85ccb76fefcaf3b0225159ca2832bca
Yakopoto <mfajarpamungkas@icloud.com>
2017-09-21 23:37:57 +0700

    wish me luck

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-'use strict'
+"use strict"
 
-const {Calculator} = require("./calculator.js")
+const Calculator = require('./calculator.js')
 
+var cal = new Calculator()
+
+cal.add(2).add(2).squareRoot()
+console.log(cal.result)
+
+//console.log(cal.result());
 // execute function on calculator.js in here
