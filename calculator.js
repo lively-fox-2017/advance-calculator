632b1bcc4760702f71f82c07cf8bfe6de2b3c7cf
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-21 14:14:56 +0700

    advanced calculate

--- calculator.js
+++ calculator.js
@@ -2,22 +2,62 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor(number) {
+    // simpan angka yang mau diproses sama kalkulator
+    this.number = number;
   }
-  add () {
+
+  add(num2) {
+    //  angka yang disimpan ditambah sama angka tertentu
+    this.number += num2;
+    return this;
   }
-  substract () {
+
+  substract(num2) {
+    // hasilnya dikurang angka yang lain
+    this.number -= num2;
+    return this;
   }
-  multiply () {
+
+  multiply(num2) {
+    // habis itu dikali pake angka lain
+    this.number *= num2;
+    return this;
   }
-  divide () {
+
+  divide(num2) {
+    // hasilnya dibagi dengan angka lain
+    this.number /= num2;
+    return this;
   }
-  square () {
+
+  square(num2) {
+    //  hasilnya dipangkat dengan angka tertentu
+    this.number = Math.pow(this.number, num2);
+    return this;
   }
-  squareRoot () {
+
+  squareRoot() {
+    // hasilnya diakar pangkat 2
+    this.number = Math.sqrt(this.number);
+    return this;
+  }
+
+  luaslingkaran() {
+    // menghitung luas lingkarang pakai fungsi Math.PI lalu hasilnya dibulatkan
+    this.number = Math.round((Math.PI * Math.pow(this.number, 2)));
+    return this;
+  }
+
+  print() {
+    // cuman buat ngeprint
+    return this.number;
   }
 }
 
+let angka1 = new Calculator(12);
+
+console.log(angka1.add(3).substract(5).multiply(8).divide(4).square(2).squareRoot().luaslingkaran().print());
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters
