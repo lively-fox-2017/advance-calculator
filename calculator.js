e7e82845b3d624c5654b55b7f13ef823335edc2e
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-09-21 14:43:26 +0700

    advanced calculator done

--- calculator.js
+++ calculator.js
@@ -2,21 +2,49 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (input) {
+    this.input = input;
   }
-  add () {
+
+  add (num) {
+    this.input += num;
+    return this;
   }
-  substract () {
+
+  substract (num) {
+    this.input -= num;
+    return this;
   }
-  multiply () {
+
+  multiply (num) {
+    this.input *= num;
+    return this;
   }
-  divide () {
+
+  divide (num) {
+    this.input /= num;
+    return this;
   }
-  square () {
+
+  square (num) {
+    this.input **= num;
+    return this;
   }
+
   squareRoot () {
+    this.input = Math.sqrt(this.input);
+    return this;
   }
 }
+
+let test = new Calculator(16);
+// console.log(test.add(2));
+// console.log(test.substract(8));
+// console.log(test.multiply(2));
+// console.log(test.divide(4));
+// console.log(test.square(4));
+// console.log(test.squareRoot());
+console.log(test.add(2).substract(8).multiply(2).divide(4).square(4).squareRoot());
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
